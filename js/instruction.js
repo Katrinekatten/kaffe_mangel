fe170e0b7ddeebd863ced8cf27c934443e074ada
Katrine <kama0011@stud.ek.dk>
2025-11-05 15:06:57 +0100

    Funktion knapper påbegyndt

--- js/instruction.js
+++ js/instruction.js
@@ -1,20 +1,26 @@
 console.log("loaded instruction...");
 
 // Overlever knap
-document.querySelector("#Overlever").addEventListener("click", kopskaleres);
-function kopskaleres() {
+document.querySelector("#Overlever").addEventListener("click", overleverf);
+function overleverf() {
   console.log("Overlever er loaded");
+  document.querySelector(".kaffekoppen").reduce;
+  10;
 }
 // Sideeffekter knap
-document.querySelector("#Sideeffekter").addEventListener("click", kaffegone);
-function kaffegone() {
+document
+  .querySelector("#Sideeffekter")
+  .addEventListener("click", sideeffekterf);
+function sideeffekterf() {
   console.log("Sideeffekter er loaded");
+  document.querySelector("#kaffe").classList.add("kaffegone");
 }
 
 // Alternativer knap
 document
   .querySelector("#Alternativer")
-  .addEventListener("click", kaffebønercolorchange);
-function kaffebønercolorchange() {
+  .addEventListener("click", alternativerf);
+function alternativerf() {
   console.log("Alternativer er loaded");
+  document.querySelector(".coffeebean").classList.add("color_change");
 }
